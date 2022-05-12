a8f965a8aa5f5e1ea17e5618c3f449eae745029b
Giang <kaylin.pham@contemi.com>
2022-05-12 23:49:51 +0700

    apply new algorithm to validate

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
-function check() {
-    const str = document.getElementById('checker').value;
-    if (Date.parse(str) && parseInt(str[0])) {
-        document.getElementById('message').innerHTML = "Valid date";
-        document.getElementById('message').classList.add('green');
-        setTimeout(() => {
-            document.getElementById('message').classList.remove('green');
-        }, 1000);
-    } else {
-        document.getElementById('message').innerHTML = "Invalid date";
-        document.getElementById('message').classList.add('red');
-        setTimeout(() => {
-            document.getElementById('message').classList.remove('red');
-        }, 1000);
-    }
+function checkValidDate() {
+  const day = document.getElementById("day").value;
+  const month = document.getElementById("month").value;
+  const year = document.getElementById("year").value;
+
+  if (
+    !check(day, "Day", 1, 31) ||
+    !check(month, "Month", 1, 12) ||
+    !check(year, "Year", 1000, 3000)
+  ) {
+    return;
+  }
+
+  showMessage(
+    `${day}/${month}/${year} is${
+      isValidDate(day, month, year) ? "" : " NOT"
+    } correct date time!`,
+    isValidDate(day, month, year) ? "green" : "red"
+  );
+}
+
+function isValidDate(day, month, year) {
+  const month31 = [1, 3, 5, 7, 8, 10, 12];
+  const month30 = [4, 6, 9, 11];
+  let maxDate;
+
+  if (month31.includes(month)) {
+    maxDate = 31;
+  } else if (month30.includes(month)) {
+    maxDate = 30;
+  } else if (year % 4 === 0 && year % 100 !== 0) {
+    maxDate = 29;
+  } else {
+    maxDate = 28;
+  }
+
+  return day <= maxDate;
+}
+
+function check(str, field, min, max) {
+  if (!str) {
+    showMessage(`Please fill in ${field}!`, "yellow");
+    return false;
+  } else if (!parseInt(str) || parseInt(str) != Number(str)) {
+    showMessage(`Input data for ${field} is incorrect format!`, "red");
+    return false;
+  } else if (parseInt(str) < min || parseInt(str) > max) {
+    showMessage(`Input data for ${field} is out of range!`, "red");
+    return false;
+  }
+
+  return true;
+}
+
+function showMessage(msg, color) {
+  document.getElementById("message").innerHTML = msg;
+  document.getElementById("message").classList.add(color);
+  setTimeout(() => {
+    document.getElementById("message").classList.remove(color);
+  }, 1500);
 }
