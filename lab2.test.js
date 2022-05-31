7ffa2f0a809781e83c53e3a45ced85c4786b1c9d
huydeve <haihuy129@gmail.com>
2022-05-31 10:35:01 +0700

    write script test [DIM:05-08] and [CD:13-21]

--- lab2.test.js
+++ lab2.test.js
@@ -1,49 +1,82 @@
 const { isValidDate, daysInMonth } = require("./lab2");
 
-const daysInMonthTestCase = [
-  { year: 2020, month: 1, expect: 31 },
-  { year: 2020, month: 3, expect: 31 },
-  { year: 2020, month: 5, expect: 31 },
-  { year: 2020, month: 7, expect: 31 },
-  { year: 2020, month: 8, expect: 31 },
-  { year: 2020, month: 10, expect: 31 },
-  { year: 2020, month: 12, expect: 31 },
-  { year: 2020, month: 4, expect: 30 },
-  { year: 2020, month: 6, expect: 30 },
-  { year: 2020, month: 9, expect: 30 },
-  { year: 2020, month: 11, expect: 30 },
-  { year: 2020, month: 2, expect: 29 },
-  { year: 2021, month: 2, expect: 28 },
-  { year: 2021, month: 0, expect: 0 },
-  { year: 2021, month: 13, expect: 0 },
-  { year: 2021, month: -1, expect: 0 },
-];
 
-const checkDateTestCase = [
-  { year: 2000, month: 1, day: 0, expect: false },
-  { year: 2000, month: 1, day: 32, expect: false },
-  { year: 2000, month: 2, day: 15, expect: false },
-  { year: 2000, month: 2, day: 15, expect: false },
-  { year: 2000, month: 2, day: 15, expect: false },
-  { year: 2000, month: 2, day: 15, expect: false },
-  { year: 2000, month: 1, day: 0, expect: false },
-  { year: 2022, month: 1, day: 31, expect: true },
-  { year: 2022, month: 3, day: 31, expect: true },
-  { year: 2022, month: 5, day: 31, expect: true },
-  { year: 2022, month: 7, day: 31, expect: true },
-  { year: 2022, month: 8, day: 31, expect: true },
-  { year: 2022, month: 10, day: 31, expect: true },
-  { year: 2022, month: 12, day: 31, expect: true },
-];
-
-daysInMonthTestCase.forEach((e) => {
-  test(`Month: ${e.month} Year: ${e.year}`, () => {
-    expect(daysInMonth(e.year, e.month)).toBe(e.expect);
+//DayInMonth
+describe('[DIM-UTCID05]', () => { 
+  it('Should return 31', () => {
+    expect(daysInMonth(2020, 8)).toBe(31)
   });
 });
 
-checkDateTestCase.forEach((e) => {
-  test(`Month: ${e.month} Year: ${e.year} Day: ${e.day}`, () => {
-    expect(isValidDate(e.year, e.month, e.day)).toBe(e.expect);
+describe('[DIM-UTCID06]', () => { 
+  it('Should return 31', () => {
+    expect(daysInMonth(2020, 10)).toBe(31)
+  });
+});
+
+describe('[DIM-UTCID07]', () => { 
+  it('Should return 31', () => {
+    expect(daysInMonth(2020, 12)).toBe(31)
+  });
+});
+
+describe('[DIM-UTCID08]', () => { 
+  it('Should return 30', () => {
+    expect(daysInMonth(2020, 4)).toBe(30)
+  });
+});
+
+///CheckDate
+describe('[CD-UTCID13]', () => { 
+  it('Should return true', () => {
+    expect(isValidDate(2400, 2, 31)).toBe(false)
+  });
+});
+
+describe('[CD-UTCID14]', () => { 
+  it('Should return false', () => {
+    expect(isValidDate(2400, 2, 30)).toBe(false)
+  });
+});
+
+describe('[CD-UTCID15]', () => { 
+  it('Should return true', () => {
+    expect(isValidDate(2400, 2, 29)).toBe(true)
+  });
+});
+
+describe('[CD-UTCID16]', () => { 
+  it('Should return false', () => {
+    expect(isValidDate(2500, 2, 29)).toBe(false)
+  });
+});
+
+describe('[CD-UTCID17]', () => { 
+  it('Should return true', () => {
+    expect(isValidDate(2500, 2, 28)).toBe(true)
+  });
+});
+
+describe('[CD-UTCID18]', () => { 
+  it('Should return true', () => {
+    expect(isValidDate(2516, 2, 29)).toBe(true)
+  });
+});
+
+describe('[CD-UTCID19]', () => { 
+  it('Should return true', () => {
+    expect(isValidDate(2516, 2, 28)).toBe(true)
+  });
+});
+
+describe('[CD-UTCID20]', () => { 
+  it('Should return false', () => {
+    expect(isValidDate(2999, 2, 29)).toBe(false)
+  });
+});
+
+describe('[CD-UTCID21]', () => { 
+  it('Should return true', () => {
+    expect(isValidDate(2999, 2, 28)).toBe(true)
   });
 });
